f5dc80b18c36b526ef81bd33d3a7926f23634db5
Zaqhry <96840072+Zaqhry@users.noreply.github.com>
2022-05-02 15:59:31 -0400

    Update SQL Movies Project.sql

--- SQL Movies Project.sql
+++ SQL Movies Project.sql
@@ -11,7 +11,13 @@ FROM Movies
 
 CREATE VIEW B AS
 (
-SELECT TOP 10 Name,Year,Rating,Genre,Budget,Gross,(Gross - Budget) Profit 
+SELECT TOP 10 Name,
+	      Year,
+	      Rating,
+	      Genre,
+	      Budget,
+	      Gross,
+	      (Gross - Budget) Profit 
 FROM Movies 
 ORDER BY Profit DESC
 )
@@ -20,7 +26,13 @@ ORDER BY Profit DESC
 
 CREATE VIEW W AS
 (
-SELECT TOP 10 Name,Year,Rating,Genre,Budget,Gross,(Gross - Budget) Profit 
+SELECT TOP 10 Name,
+	      Year,
+	      Rating,
+	      Genre,
+	      Budget,
+	      Gross,
+	      (Gross - Budget) Profit 
 FROM Movies 
 WHERE Gross < Budget
 ORDER BY Profit 
@@ -28,7 +40,7 @@ ORDER BY Profit
 
 
 
---Top 10 Best Selling vs Worst Selling Movies Comparison 
+--Top 10 Most Profitable vs Least Profitable Movies Comparison 
 
 SELECT * 
 FROM B 
@@ -58,21 +70,21 @@ FROM Movies
 --Score,Genre,Rating,Budget,Gross,Profit,Runtime For Top 10 Best Movies vs Average Across All Movies 
 
 SELECT TOP 10 Year,
-			  Name,
-			  director,
-			  writer,
-			  Genre,
-	          Score,
-			  Rating,
-	          (SELECT ROUND(AVG(Score),2) FROM Movies) AvgScore,
-	          Budget,
-	          (SELECT ROUND(AVG(Budget),2) FROM Movies) AvgBudget,
-	          Gross,
-	         (SELECT ROUND(AVG(Gross),2) FROM Movies) AvgGross,
-		     (Gross - Budget) Profit,
-		     (SELECT ROUND(AVG(Gross - Budget),2) FROM Movies) AvgProfit,
-		     Runtime,
-		     (SELECT ROUND(AVG(Runtime),2) FROM Movies) AvgRuntime
+	      Name,
+	      director,
+	      writer,
+	      Genre,
+	      Score,
+	      Rating,
+	      (SELECT ROUND(AVG(Score),2) FROM Movies) AvgScore,
+	      Budget,
+	      (SELECT ROUND(AVG(Budget),2) FROM Movies) AvgBudget,
+	      Gross,
+	      (SELECT ROUND(AVG(Gross),2) FROM Movies) AvgGross,
+	      (Gross - Budget) Profit,
+	      (SELECT ROUND(AVG(Gross - Budget),2) FROM Movies) AvgProfit,
+	      Runtime,
+	      (SELECT ROUND(AVG(Runtime),2) FROM Movies) AvgRuntime
 FROM Movies 
 ORDER BY Profit DESC
 
@@ -81,21 +93,21 @@ ORDER BY Profit DESC
 --Score,Genre,Rating,Budget,Gross,Profit,Runtime For Top 10 Worst Movies vs Average Across All Movies 
 
 SELECT TOP 10 Year,
-			  Name,
-		      director,
-			  writer,
-			  Genre,
-	          Score,
-			  Rating,
-	          (SELECT ROUND(AVG(Score),2) FROM Movies) AvgScore,
-	          Budget,
-	          (SELECT ROUND(AVG(Budget),2) FROM Movies) AvgBudget,
-	          Gross,
-	         (SELECT ROUND(AVG(Gross),2) FROM Movies) AvgGross,
-		     (Gross - Budget) Profit,
-		     (SELECT ROUND(AVG(Gross - Budget),2) FROM Movies) AvgProfit,
-		     Runtime,
-		     (SELECT ROUND(AVG(Runtime),2) FROM Movies) AvgRuntime
+	      Name,
+              director,
+	      writer,
+	      Genre,
+              Score,
+	      Rating,
+              (SELECT ROUND(AVG(Score),2) FROM Movies) AvgScore,
+              Budget,
+              (SELECT ROUND(AVG(Budget),2) FROM Movies) AvgBudget,
+              Gross,
+              (SELECT ROUND(AVG(Gross),2) FROM Movies) AvgGross,
+              (Gross - Budget) Profit,
+              (SELECT ROUND(AVG(Gross - Budget),2) FROM Movies) AvgProfit,
+              Runtime,
+              (SELECT ROUND(AVG(Runtime),2) FROM Movies) AvgRuntime
 FROM Movies 
 WHERE Gross < Budget
 ORDER BY Profit 
@@ -107,9 +119,9 @@ ORDER BY Profit
 --Movies that made a negative profit 
 
 SELECT Name,
-	   Budget,
-	   Gross,
-	   (Gross - Budget) Profit 
+       Budget,
+       Gross,
+       (Gross - Budget) Profit 
 FROM Movies 
 WHERE Gross < Budget
 ORDER BY Profit 
@@ -119,31 +131,31 @@ ORDER BY Profit
 --Profit made for each movie by Writer & Director 
 
 SELECT name,
-	   writer,
-	   director,
-	   SUM(Gross - Budget) Profit 
+       writer,
+       director,
+       SUM(Gross - Budget) Profit 
 FROM Movies 
 	GROUP BY name,
-		     writer,
-		     director
+	         writer,
+	         director
 	ORDER BY Profit DESC
 
 
 
---How much money Writers have made with all movies combined 
+--How much profit Writers have made with all movies combined 
 
 SELECT writer,
-	   SUM(Gross - Budget) Profit 
+       SUM(Gross - Budget) Profit 
 FROM Movies 
 	GROUP BY writer
 	ORDER BY Profit DESC
 
 
 
---How much money Directors have made with all movies combined
+--How much profit Directors have made with all movies combined
 
 SELECT director,
-	   SUM(Gross - Budget) Profit 
+       SUM(Gross - Budget) Profit 
 FROM Movies 
 	GROUP BY director
 	ORDER BY Profit DESC
@@ -153,7 +165,7 @@ FROM Movies
 --Number of movies for each writer 
 
 SELECT Writer,
-	   COUNT(*) Movies
+       COUNT(*) Movies
 FROM Movies
 	GROUP BY Writer
 	ORDER BY 2 DESC
@@ -163,7 +175,7 @@ FROM Movies
 --Number of movies for each director 
 
 SELECT Director,
-	   COUNT(*) Movies
+       COUNT(*) Movies
 FROM Movies
 	GROUP BY Director
 	ORDER BY 2 DESC
@@ -173,7 +185,7 @@ FROM Movies
 --Budget for each movie 
 
 SELECT Name,
-	   Max(Budget) Budget
+       Max(Budget) Budget
 FROM Movies
 WHERE Budget IS NOT NULL
 	GROUP BY Name
@@ -184,7 +196,7 @@ WHERE Budget IS NOT NULL
 --Budget for each genre
 
 SELECT Genre,
-	   Max(Budget) Budget
+       Max(Budget) Budget
 FROM Movies
 WHERE Budget IS NOT NULL
 	GROUP BY Genre
@@ -194,36 +206,37 @@ WHERE Budget IS NOT NULL
 
 --Profit for each movie 
 
-SELECT Name,
-	  (Gross - Budget) Profit
+SELECT Name,  
+       (Gross - Budget) Profit
 FROM Movies
 	ORDER BY Profit DESC
 
 
---Company that has made the most movies 
+
+--# of movies made by each company 
 
 SELECT Company,
-	   COUNT(Name) TotalMovies 
+       COUNT(Name) TotalMovies 
 FROM Movies 
 	GROUP BY Company
 	ORDER BY TotalMovies DESC
 
 
 
---Most movies made per genre 
+--# of movies made per genre 
 
 SELECT Genre,
-	   COUNT(Name) TotalMovies 
+       COUNT(Name) TotalMovies 
 FROM Movies 
 	GROUP BY Genre
 	ORDER BY TotalMovies DESC
 
 
 
---Most movies made per rating
+--# of movies made per rating
 
-SELECT Rating,
-	   COUNT(Name) TotalMovies 
+SELECT Rating, 
+       COUNT(Name) TotalMovies 
 FROM Movies 
 WHERE Rating IS NOT NULL
 	GROUP BY Rating
@@ -231,24 +244,26 @@ WHERE Rating IS NOT NULL
 
 
 
---Company that has made the most profit off of all movies 
+--Profit made by each company 
 	
 SELECT Company,
-	   COUNT(Name) TotalMovies,SUM(Gross - Budget) TotalProfit 
+       COUNT(Name) TotalMovies,SUM(Gross - Budget) TotalProfit 
 FROM Movies 
 	GROUP BY Company 
 	ORDER BY TotalMovies DESC
 
 
 
---Movies with the highest profit ratio 
+--Profit Ratio for movies 
 
 SELECT Name, 
-	   Budget,
-	   Gross,
-	   ROUND(MAX(Gross - Budget) / MIN(Budget),2) ProfitRatio 
+       Budget,
+       Gross,
+       ROUND(MAX(Gross - Budget) / MIN(Budget),2) ProfitRatio 
 FROM Movies 
-	GROUP BY Name, Budget,Gross
+	GROUP BY Name, 
+	         Budget,
+		 Gross
 	ORDER BY ProfitRatio DESC
 	
 
@@ -256,11 +271,13 @@ FROM Movies
 --TOP 5 Movies with lowest budget but highest profit return 
 
 SELECT TOP 5 Name, 
-	   Budget,
-	   Gross,
-	   ROUND(MAX(Gross - Budget) / MIN(Budget),2) ProfitRatio 
+	     Budget,
+	     Gross,
+	     ROUND(MAX(Gross - Budget) / MIN(Budget),2) ProfitRatio 
 FROM Movies 
-	GROUP BY Name, Budget,Gross
+	GROUP BY Name, 
+	         Budget,
+		 Gross
 	ORDER BY ProfitRatio DESC
 	
 	
@@ -268,7 +285,7 @@ FROM Movies
 --Number of movies produced by countries 
 
 SELECT Country,
-	   COUNT(Name) TotalMovies 
+       COUNT(Name) TotalMovies 
 FROM Movies 
 	GROUP BY Country
 	ORDER BY TotalMovies DESC
@@ -278,7 +295,7 @@ FROM Movies
 --Year with the most movies produced
 
 SELECT Year,
-	   COUNT(Name) TotalMovies
+       COUNT(Name) TotalMovies
 FROM Movies 
 	GROUP BY Year 
 	ORDER BY TotalMovies DESC
@@ -287,8 +304,8 @@ FROM Movies
 
 --Rating that most movies are labeled under 
 
-SELECT Rating,
-	   COUNT(Rating) TotalRating
+SELECT Rating, 
+       COUNT(Rating) TotalRating
 FROM Movies 
 WHERE Rating IS NOT NULL
 	GROUP BY Rating 
@@ -299,10 +316,11 @@ WHERE Rating IS NOT NULL
 --Runtime For Movies vs Average Runtime For All Movies 
 
 SELECT Name,
-	   Runtime,
-	   (SELECT ROUND(AVG(Runtime),2) FROM Movies) Avg_Runtime_For_All_Movies
+       Runtime,
+       (SELECT ROUND(AVG(Runtime),2) FROM Movies) Avg_Runtime_For_All_Movies
 FROM Movies
-	GROUP BY Name,Runtime
+	GROUP BY Name,
+	         Runtime
 	ORDER BY Runtime DESC
 
 
@@ -310,8 +328,8 @@ FROM Movies
 --Total & Avg Profit Per Genre (Most profitable genre) 
 
 SELECT Genre,
-	   SUM(Gross - Budget) TotalProfit,
-	   ROUND(AVG(Gross - Budget),2) AvgProfit 
+       SUM(Gross - Budget) TotalProfit,
+       ROUND(AVG(Gross - Budget),2) AvgProfit 
 FROM Movies 
 	GROUP BY Genre
 	ORDER BY TotalProfit DESC
@@ -321,9 +339,9 @@ FROM Movies
 --Total & Avg Profit Per Rating (Most profitable Rating) 
 
 
-SELECT Rating,
-	   SUM(Gross - Budget) TotalProfit,
-	   ROUND(AVG(Gross - Budget),2) AvgProfit 
+SELECT Rating, 
+       SUM(Gross - Budget) TotalProfit,
+       ROUND(AVG(Gross - Budget),2) AvgProfit 
 FROM Movies 
 WHERE Rating IS NOT NULL
 	GROUP BY Rating
@@ -334,8 +352,8 @@ WHERE Rating IS NOT NULL
 --Total & Avg Profit Per Year (Most profitable Year) 
 
 SELECT Year,
-	   SUM(Gross - Budget) TotalProfit,
-	   ROUND(AVG(Gross - Budget),2) AvgProfit 
+       SUM(Gross - Budget) TotalProfit,
+       ROUND(AVG(Gross - Budget),2) AvgProfit 
 FROM Movies 
 	GROUP BY Year
 	ORDER BY TotalProfit DESC
@@ -345,8 +363,8 @@ FROM Movies
 --Total & Avg Profit Per Country (Most profitable Country) 
 
 SELECT Country,
-	   SUM(Gross - Budget) TotalProfit,
-	   ROUND(AVG(Gross - Budget),2) AvgProfit 
+       SUM(Gross - Budget) TotalProfit,
+       ROUND(AVG(Gross - Budget),2) AvgProfit 
 FROM Movies 
 	GROUP BY Country
 	ORDER BY TotalProfit DESC
@@ -356,8 +374,8 @@ FROM Movies
 --Total & Avg Profit Per Movie (Most profitable Movie) 
 
 SELECT Name,
-	   SUM(Gross - Budget) TotalProfit,
-	   ROUND(AVG(Gross - Budget),2) AvgProfit 
+       SUM(Gross - Budget) TotalProfit,
+       ROUND(AVG(Gross - Budget),2) AvgProfit 
 FROM Movies 
 	GROUP BY Name
 	ORDER BY TotalProfit DESC
@@ -367,8 +385,11 @@ FROM Movies
 --Total & Avg Profit Per Scores (Most Profitable Scores) 
 
 SELECT Score,
-	   SUM(Gross - Budget) TotalProfit,
-	   ROUND(AVG(Gross - Budget),2) AvgProfit 
+       SUM(Gross - Budget) TotalProfit,
+       ROUND(AVG(Gross - Budget),2) AvgProfit 
 FROM Movies 
 	GROUP BY Score
 	ORDER BY TotalProfit DESC
+	
+	
+	
